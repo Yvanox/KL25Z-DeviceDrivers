568234827be66ff846e146f3bc0e2894b40c4ca7
Yvanox <yvanramireza@gmail.com>
2018-08-12 23:21:54 -0500

    Start of SPI Device Drivers

---
+++ SPIdrivers/source/SPI.h
@@ -0,0 +1,24 @@
+/*
+ * SPI.h
+ *
+ *  Created on: May 12, 2018
+ *      Author: yvanox
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+#include "MKL25Z4.h"
+
+/*********************************************************************
+ * 																	 *
+ * 					SPI Register Bit Definitions					 *
+ * 																	 *
+ *********************************************************************/
+
+/*****************SPI Control Register 1 (SPIx_C1)********************/
+
+
+
+
+#endif /* SPI_H_ */
